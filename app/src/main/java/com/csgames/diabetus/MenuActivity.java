03714fead2ff1918d6e7e4ba3d0611bc05609a50
jacobrs <jacob_gagne@hotmail.com>
2016-02-14 16:35:35 -0500

    linked quiz button

--- app/src/main/java/com/csgames/diabetus/MenuActivity.java
+++ app/src/main/java/com/csgames/diabetus/MenuActivity.java
@@ -1,10 +1,15 @@
 package com.csgames.diabetus;
 
+import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    public final MenuActivity parent = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +17,14 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
         Snackbar.make(findViewById(android.R.id.content), "Connected to Diabetus",
                 Snackbar.LENGTH_SHORT).show();
+
+        ((Button) findViewById(R.id.quiz)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent quiz = new Intent(parent, QuizActivity.class);
+                startActivity(quiz);
+            }
+        });
     }
 
     @Override
